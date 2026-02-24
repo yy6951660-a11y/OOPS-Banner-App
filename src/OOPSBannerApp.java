b0c5936b2c137cbf4af58bc559cfd27ab155c8b5
yy6951660-a11y <yy6951660@gmail.com>
2026-02-24 12:57:13 +0530

    Added UC3  printing OOPS Banner using sout with *

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,17 @@
+ //version 3.0
+public class OOPSBannerApp {
+     public static void main(String[] args) {
+    System.out.println(String.join("  ","   *   ","   *   ","**   ","   * * * "));
+    System.out.println(String.join("  "," *   * "," *   * ","*    * ","*     "));
+    System.out.println(String.join("  ","*     *","*     *","*     *","*       "));
+    System.out.println(String.join("  ","*     *","*     *","*    * ","*        "));
+    System.out.println(String.join("  ","*     *","*     *","**   ","   *   "));
+    System.out.println(String.join("  ","*     *","*     *","*       ","      * "));
+    System.out.println(String.join("  ","*     *","*     *","*       ","       *"));
+    System.out.println(String.join("  "," *   * "," *   * ","*       ","      * "));
+    System.out.println(String.join("  ","   *   ","   *   ","*       "," * *   "));
+    System.out.println("                                                               ");
+     }
+}
+    
+
